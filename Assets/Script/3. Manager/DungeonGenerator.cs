fc8e7857fd6f130976503898facb687f51d45d47
Udi <dnel0605@gmail.com>
2025-03-25 11:40:24 +0900

    [feat] 난간 자동 생성
    난간 자동 생성하는 기능 추가

--- Assets/Script/3. Manager/DungeonGenerator.cs
+++ Assets/Script/3. Manager/DungeonGenerator.cs
@@ -29,8 +29,8 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] private int _dungeonDepth;
 
     // Dungeon Node
-    private List<Vector3> nodes = new List<Vector3>();
-    private List<(int, int)> edges = new List<(int, int)>();
+    private List<Vector3> _nodes = new List<Vector3>();
+    private List<(int, int)> _edges = new List<(int, int)>();
     void Awake()
     {
         GenerateDungeon();
@@ -48,12 +48,16 @@ public class DungeonGenerator : MonoBehaviour
         // 노드 연결
         ConnectNodes();
 
-        Invoke(nameof(GeneratePath), 5f);
+        // 노드 경로 생성
+        GeneratePath(); 
+
+        // 난간 설치
+        GenerateRailing();
     }
 
     private void CreateNode()
     {
-        nodes.Clear(); 
+        _nodes.Clear(); 
         bool [,] _visited = new bool[(int)_dungeonSize.x, (int)_dungeonSize.y];
 
         int nodeCnt = _dungeonDepth;
@@ -64,7 +68,7 @@ public class DungeonGenerator : MonoBehaviour
 
             if (!_visited[x, z])
             {
-                nodes.Add(new Vector3(x, 0, z)*8); 
+                _nodes.Add(new Vector3(x, 0, z)); 
                 _visited[x, z] = true;
                 nodeCnt--;
             } 
@@ -73,9 +77,9 @@ public class DungeonGenerator : MonoBehaviour
 
     private void PlaceNodes()
     {
-        foreach (var node in nodes)
+        foreach (var node in _nodes)
         {
-            GameObject floor1 = Instantiate(_floor.prefab, node - new Vector3(_floor.sizeX, 0, _floor.sizeZ), Quaternion.identity);
+            GameObject floor1 = Instantiate(_floor.prefab, node*8 - new Vector3(_floor.sizeX, 0, _floor.sizeZ), Quaternion.identity);
         }
     }
 
@@ -84,20 +88,20 @@ public class DungeonGenerator : MonoBehaviour
         // from, to, cost ==> ex((1, 2, 3) => 1에서 2로 가는 거리는 3)
         List<(int, int, int)> tempEdges = new List<(int, int, int)>();
         // 최종 간선을 담을 List
-        edges.Clear();
+        _edges.Clear();
 
         // 비용 초기화
-        for (int i = 0; i < nodes.Count; i++)
-            for (int j = i+1; j < nodes.Count; j++)
-                tempEdges.Add((i, j, (int)Vector3.Distance(nodes[i], nodes[j])));
+        for (int i = 0; i < _nodes.Count; i++)
+            for (int j = i+1; j < _nodes.Count; j++)
+                tempEdges.Add((i, j, (int)Vector3.Distance(_nodes[i], _nodes[j])));
             
         // 비용 오름차순 정렬
         tempEdges.Sort((a, b) => a.Item3.CompareTo(b.Item3));
 
         // 크루스칼 알고리즘을 통해 최소 간선만 남기기
         // Union - Find 알고리즘을 통해 간선 연결 여부 파악
-        int[] parents = new int[nodes.Count];
-        for (int i = 0; i < nodes.Count; i++)
+        int[] parents = new int[_nodes.Count];
+        for (int i = 0; i < _nodes.Count; i++)
             parents[i] = i;
 
 
@@ -111,18 +115,18 @@ public class DungeonGenerator : MonoBehaviour
                 continue; 
 
             Union(parents, From, To);
-            edges.Add((From, To));
+            _edges.Add((From, To));
         }
     } 
 
     private void GeneratePath()
     {
         HashSet<Vector3> visited = new HashSet<Vector3>();
-        foreach (var edge in edges)
+        foreach (var edge in _edges)
         {
             var (From, To) = edge;
-            Vector3 start = nodes[From] / 8;
-            Vector3 end = nodes[To] / 8;
+            Vector3 start = _nodes[From];
+            Vector3 end = _nodes[To];
 
             // 간단한 방법으로 길찾기 수행
             // 장애물이 없는 상황이기 때문에 상관 X
@@ -163,10 +167,61 @@ public class DungeonGenerator : MonoBehaviour
                 visited.Add(start);
 
                 GameObject floor1 = Instantiate(_floor.prefab, start*8 - new Vector3(_floor.sizeX, 0, _floor.sizeZ), Quaternion.identity);
-            
+                _nodes.Add(start); 
             }
         } 
     } 
+
+    private void GenerateRailing()
+    {
+        HashSet<Vector2> nodeHash = new HashSet<Vector2>();
+
+        foreach (var node in _nodes)
+            nodeHash.Add(new Vector2(node.x, node.z));
+ 
+        for (int i = 0; i < _nodes.Count; i++)
+        {
+            Vector3 curNode = _nodes[i] * 8; 
+            curNode -= new Vector3(_floor.sizeX, 0, _floor.sizeZ);
+
+
+            // 왼쪽에 다른 노드가 있는가?
+            if (!nodeHash.Contains(new Vector2(_nodes[i].x - 1, _nodes[i].z)))
+            {
+                var go = Instantiate(railing.prefab, curNode + new Vector3(-(_floor.sizeX/2), 0, _floor.sizeZ/2), Quaternion.identity); 
+                go.transform.localScale = new Vector3(1, 1, 1);
+                go.transform.eulerAngles = new Vector3(0, 90, 0);
+            }
+
+            // 오른쪽에 다른 노드가 있는가? 
+            if (!nodeHash.Contains(new Vector2(_nodes[i].x + 1, _nodes[i].z)))
+            {
+                var go = Instantiate(railing.prefab, curNode + new Vector3(_floor.sizeX/2, 0, _floor.sizeZ/2), Quaternion.identity); 
+                go.transform.localScale = new Vector3(1, 1, 1);
+                go.transform.eulerAngles = new Vector3(0, -90, 0);
+            }
+
+            // 위에 다른 노드가 있는가?
+            if (!nodeHash.Contains(new Vector2(_nodes[i].x, _nodes[i].z + 1)))
+            {
+                var go = Instantiate(railing.prefab, curNode + new Vector3(0, 0, _floor.sizeZ), Quaternion.identity); 
+                go.transform.localScale = new Vector3(-1, 1, 1);
+            }
+ 
+            // 아래에 다른 노드가 있는가?
+            if (!nodeHash.Contains(new Vector2(_nodes[i].x, _nodes[i].z - 1)))
+            {
+                Instantiate(railing.prefab, curNode, Quaternion.identity); 
+            }
+        }
+
+        // 난간 설치 알고리즘
+        // DFS를 사용해서 모든 노드에 대해서 난간 설치
+        // 1. 해당 노드의 상하좌우에 인접한 다른 노드가 있는지 체크한다
+        // 2. 인접한 노드가 없는경우에만 해당 방향에 난간을 설치한다
+
+
+    }
 
     private void Union(int[] parents, int a, int b)
     {
